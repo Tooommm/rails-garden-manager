1e1df2cc23832212d842800083466b80fadef00d
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-06-09 15:05:22 +0200

    put regex for img url

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,6 +1,7 @@
 class Garden < ApplicationRecord
+  URL_REGEX = /(https?:\/\/.*\.(?:png|jpg))/i
   has_many :plants, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :banner_url, presence: true
+  validates :banner_url, presence: true, format: { with: URL_REGEX, message: "please enter a correct image url" }
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,7 @@
 class Plant < ApplicationRecord
+  URL_REGEX = /(https?:\/\/.*\.(?:png|jpg))/i
   belongs_to :garden
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
-  validates :image_url, presence: true
+  validates :image_url, presence: true, format: { with: URL_REGEX, message: "please enter a correct image url" }
 end
